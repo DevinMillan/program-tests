90c8bedad17dbec52b9c065449e38b3d83f38ff4
DevinMillan <149868946+DevinMillan@users.noreply.github.com>
2024-11-20 23:44:28 +0800

    Update main.py
    Added a nested for-loop

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ int x = 1
 while(x<5):
     print("This is a while loop")
 
+# nest for-loop
 y = ["1","2","3"]
 for x in y:
-    print(x)
-    if x == "2":
-        break
+    for x in y;
+        print(x)
+        if x == "2":
+            break
         
 for x in range(6):
     print("This is range")
