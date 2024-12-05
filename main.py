762eb73bfd7d0003a2b901e0daa706bf0c941733
DevinMillan <149868946+DevinMillan@users.noreply.github.com>
2024-12-05 23:05:27 +0800

    Update main.py
    Changes on comments, fixed code syntax, and initialised a string value at nested methods

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ int x = 1 # Initialization of integer x
 while(x<5):
     print("This is a while loop")
     while(x<2):
-        print("This is a nested while loop")
+        print("This is now a nested while loop")
 
 # nested for-loop
 y = ["1","2","3"]
 for x in y:
-    for x in y;
+    for x in y:
         print(x)
         if x == "2":
             break
@@ -30,7 +30,8 @@ def Function():
     if(y>4):
         print("I made a method with a if statement!")
     else:
-        print("Else was executed")
+        print("Else statanent executed")
     def AnotherFunction(x):
         print("Nested Method")
+        String x = "Hello"
         return x;
